4deed9c19e8d6aa96885a4827185396cd28b86ad
姜守栋 <18511632756@163.com>
2018-04-17 09:53:15 +0800

    初始化工程

---
+++ JSDStarScoreView/JSDStarScoreView/StarScoreView.swift
@@ -0,0 +1,23 @@
+//
+//  StarScoreView.swift
+//  JSDStarScoreView
+//
+//  Created by 姜守栋 on 2018/4/17.
+//  Copyright © 2018年 Nick.Inc. All rights reserved.
+//
+
+import UIKit
+
+class StarScoreView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
---
+++ JSDStarScoreView/JSDStarScoreView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSDStarScoreView
+//
+//  Created by 姜守栋 on 2018/4/17.
+//  Copyright © 2018年 Nick.Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
